5093c286e105e596bf0e024f72ca191d0011dc7b
Rolfvanderplas <31855091+Rolfvanderplas@users.noreply.github.com>
2025-09-17 19:18:18 +0200

    add mosquitto permissions

--- post_install_script.sh
+++ post_install_script.sh
@@ -1,6 +1,9 @@
-# Set credentials right for system
-
-sudo mkdir -p /mnt/external/appdata/{heimdall,portainer,pihole/config,pihole/dnsmasq.d,pyload,monitoring/telegraf,influxdb,grafana/data,monitoring/grafana}
-sudo chown -R 1000:1000 /mnt/external/appdata/*        # except grafana/data
+# Set ownership for all directories
+sudo chown -R 1000:1000 /mnt/external/appdata/heimdall /mnt/external/appdata/portainer /mnt/external/appdata/pihole /mnt/external/appdata/pyload /mnt/external/appdata/monitoring /mnt/external/appdata/influxdb /mnt/external/appdata/grafana/data
 sudo chown -R 472:472 /mnt/external/appdata/grafana/data
+
+# Set permissions recursively to 755 for all directories and files
 sudo chmod -R 755 /mnt/external/appdata
+
+# (Optional) set specific permissions for specific files/directories
+sudo chown -R 1883:1883 /mnt/external/appdata/mosquitto/config
