da18dddfb4724168c0cfdf6fb8dda5e926aab5ea
Rolfvanderplas <31855091+Rolfvanderplas@users.noreply.github.com>
2025-09-18 20:54:56 +0200

    mosquitto settings

--- post_install_script.sh
+++ post_install_script.sh
@@ -5,5 +5,8 @@ sudo chown -R 472:472 /mnt/external/appdata/grafana/data
 # Set permissions recursively to 755 for all directories and files
 sudo chmod -R 755 /mnt/external/appdata
 
-# (Optional) set specific permissions for specific files/directories
-sudo chown -R 1883:1883 /mnt/external/appdata/mosquitto/config
+# mosquitto settings
+sudo mkdir -p /mnt/external/appdata/mosquitto/config && \
+echo -e "listener 1883\nallow_anonymous true" | sudo tee /mnt/external/appdata/mosquitto/config/mosquitto.conf > /dev/null && \
+sudo chown 1000:1000 /mnt/external/appdata/mosquitto/config/mosquitto.conf && \
+sudo chmod 644 /mnt/external/appdata/mosquitto/config/mosquitto.conf
